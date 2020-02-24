71510205ed674cf4abebc384eecce24131a9f327
kkreinheder <47006009+kkreinheder@users.noreply.github.com>
2020-02-24 08:37:00 -0500

    Create Test.py

---
+++ back-end/Test.py
@@ -0,0 +1,12 @@
+import pika
+
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+channel = connection.channel()
+channel.queue_declare(queue='test')
+
+channel.basic_publish(exchange='', routing_key='test', body='Message')
+connection.close()
+
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+channel = connection.channel()
+method_frame, header_frame, body = channel.basic_get('test')
