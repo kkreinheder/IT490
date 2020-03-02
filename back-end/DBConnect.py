15f08ccd892c78da29c18beedf387250b66f4c05
kkreinheder <47006009+kkreinheder@users.noreply.github.com>
2020-03-01 23:25:18 -0500

    Update DBConnect.py

--- back-end/DBConnect.py
+++ back-end/DBConnect.py
@@ -6,9 +6,16 @@ db = pymysql.connect("0.0.0.0", "test", "test", "db-mysql")
 
 cursor = db.cursor()
 
-sql = "INSERT INTO user(first_name, last_name, email)
-        VALUES (John, Smith, jsmith@gmail.com)"
+write = "INSERT INTO user (first_name, last_name, email) VALUES (John, Smith, jsmith@gmail.com)"
         
-sql = "SELECT * FROM user"
+read = "SELECT * FROM user"
+
+try:
+    cursor.execute(write)
+    cursor.execute(read)
+    db.commit()
+    
+except:
+    db.rollback()
 
 db.close()
