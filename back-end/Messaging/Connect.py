14fb6da4ca7d1519a9bb98155c46a7f17f4ba08c
kkreinheder <47006009+kkreinheder@users.noreply.github.com>
2020-03-11 15:24:38 -0400

    Update Connect.py

--- back-end/Messaging/Connect.py
+++ back-end/Messaging/Connect.py
@@ -7,6 +7,6 @@ channel.queue_declare(queue='test')
 channel.basic_publish(exchange='', routing_key='test', body='Message')
 connection.close()
 
-connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='172.18.0.3'))
 channel = connection.channel()
 method_frame, header_frame, body = channel.basic_get('test')
